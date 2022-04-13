7d619004f77daa196a1703b204f70141ecb8a606
EdwardTulane1 <opps.zbeda@gmail.com>
2022-04-13 08:19:48 +0300

    files

--- macro.h
+++ macro.h
@@ -1,22 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "structs.h"
+#include "enums.h"
 
 #ifndef MACRO_H
 #define MACRO_H
 
 #define MAX_LINE_LENGTH 80
 
-
-typedef enum boolean {
-  FALSE = 0,
-  TRUE = 1
-} bool;
-
-typedef struct macros {
-    char macro_name[MAX_LINE_LENGTH];
-    char * macro_lines;
-} macro;
 
 void replace_multi_space_with_single_space(char *);
 bool validReg(char *);
