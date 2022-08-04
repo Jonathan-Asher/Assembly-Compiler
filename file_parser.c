2954179ac8f818f8197a29c128833517de3f4e3e
EdwardTulane1 <opps.zbeda@gmail.com>
2022-08-04 20:43:31 +0300

    zz

--- file_parser.c
+++ file_parser.c
@@ -17,6 +17,11 @@ int parse_file(char * filename) {
   ssize_t read;
   int line_num = 1;
   int i;
+  bool labelExists=false;
+  symbol_table a1;
+  a1.head=NULL;
+
+
 
   strncpy(dest_filename, filename, strlen(filename) - 4);
   dest_filename[strlen(filename) - 4] = '\0';
@@ -45,6 +50,25 @@ int parse_file(char * filename) {
         printf("\tcell [%d]: '%s' ", i+1, current_line->parsed_line[i]);
     }
     current_line->type = get_line_type(current_line);
+
+    if(validLabel(current_line->parsed_line[0])){
+      labelExists=true;
+    }
+
+    switch (current_line->type)
+    {
+    case INSTRUCTION_LINE:
+      label_type ltype = get_inst_type(current_line);
+      add_label(current_line->parsed_line[0], dc, label_type, a1)
+
+    case COMMAND_LINE:
+      label_type ltype = get_cmd_type(current_line);
+    case ERROR_LINE:
+    default:
+      break;
+    }
+
+
     printf("\n type %d \n", current_line->type);
     if(current_line->type == 3) {
       label_type ltype = get_inst_type(current_line);
